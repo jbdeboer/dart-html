f4496d5cabfae7d1c13cdd21dfc70f49463d532c
James deBoer <james@huronbox.com>
2013-04-15 11:21:54 -0700

    di dependency

---
+++ test/fixed-unittest.dart
@@ -0,0 +1,108 @@
+library unittest;
+
+import 'package:unittest/unittest.dart';
+import 'package:di/mirrors.dart';
+
+export 'package:unittest/unittest.dart';
+
+// fix the testing framework ;-)
+void it(String spec, TestFunction body) => test(spec, body);
+void xit(String spec, TestFunction body) {}
+void iit(String spec, TestFunction body) => solo_test(spec, body);
+
+Matcher toEqual(expected) => equals(expected);
+Matcher toBe(expected) => same(expected);
+Matcher instanceOf(Type t) => new IsInstanceOfTypeMatcher(t);
+
+Matcher toThrow(Type exceptionClass, String message) =>
+  new ThrowsMatcher(new ComplexExceptionMatcher(instanceOf(exceptionClass), toEqual(message)));
+
+Matcher not(Matcher matcher) => new NegateMatcher(matcher);
+
+
+class NegateMatcher extends BaseMatcher {
+  final Matcher _matcher;
+
+  const NegateMatcher(Matcher matcher) : _matcher = matcher;
+
+  bool matches(obj, MatchState ms) {
+    return !_matcher.matches(obj, ms);
+  }
+
+  Description describe(Description description) {
+    description.add('NOT ');
+    return _matcher.describe(description);
+  }
+
+  Description describeMismatch(item, Description mismatchDescription, MatchState matchState,
+                               bool verbose) {
+    return _matcher.describeMismatch(item, mismatchDescription, matchState, verbose);
+  }
+}
+
+
+class ThrowsMatcher extends Throws {
+  final Matcher _matcher;
+
+  const ThrowsMatcher([Matcher matcher]) : _matcher = matcher, super(matcher);
+
+  Description describeMismatch(item, Description mismatchDescription,
+                               MatchState matchState,
+                               bool verbose) {
+    if (item is! Function && item is! Future) {
+      return mismatchDescription.add(' not a Function or Future');
+    }
+
+    if (_matcher == null ||  matchState.state == null) {
+      return mismatchDescription.add(' did not throw any exception');
+    }
+
+    return _matcher.describeMismatch(item, mismatchDescription, matchState, verbose);
+  }
+}
+
+class ComplexExceptionMatcher extends BaseMatcher {
+  Matcher classMatcher;
+  Matcher messageMatcher;
+
+  ComplexExceptionMatcher(this.classMatcher, this.messageMatcher);
+
+  bool matches(obj, MatchState ms) {
+    if (!classMatcher.matches(obj, ms)) {
+      return false;
+    }
+
+    return messageMatcher.matches(obj.message, ms);
+  }
+
+  Description describe(Description description) {
+    classMatcher.describe(description);
+
+    description.add(' with message ');
+    messageMatcher.describe(description);
+  }
+
+  Description describeMismatch(item, Description mismatchDescription, MatchState matchState,
+                               bool verbose) {
+    Exception e = matchState.state['exception'];
+    mismatchDescription.add('threw ').addDescriptionOf(e).add(' with message ').addDescriptionOf(e.message);
+  }
+
+}
+
+// Welcome to Dart ;-)
+class IsInstanceOfTypeMatcher extends BaseMatcher {
+  Type t;
+  
+  IsInstanceOfTypeMatcher(Type t) {
+    this.t = t;
+  }
+  
+  bool matches(obj, MatchState matchState) {
+    // we should at least compare qualifiedName, but there's no way to get it from Type
+    return reflect(obj).type.simpleName == t.toString();
+  }
+  
+  Description describe(Description description) =>
+    description.add('an instance of ${t.toString()}');
+}
---
+++ test/main.dart
@@ -0,0 +1,246 @@
+import 'fixed-unittest.dart';
+import 'package:di/di.dart';
+
+// just some classes for testing
+class Abc {
+  String id = 'abc-id';
+  
+  void greet() {
+    print('HELLO');
+  }
+}
+
+class MockAbc implements Abc {
+  String id = 'mock-id';
+  
+  void greet() {
+    print('MOCK HELLO');
+  }
+}
+
+class Complex {
+  Abc value;
+  
+  Complex(Abc val) {
+    value = val;
+  }
+  
+  dynamic getValue() {
+    return value;
+  }
+}
+
+class NumDependency {
+  NumDependency(num value) {}
+}
+
+class StringDependency {
+  StringDependency(String value) {}
+}
+
+class BoolDependency {
+  BoolDependency(bool value) {}
+}
+
+
+class CircularA {
+  CircularA(CircularB b) {}
+}
+
+class CircularB {
+  CircularB(CircularA a) {}
+}
+
+// pretend, you don't see this main method
+void main() {
+  
+it('should instantiate a type', () {
+  var injector = new Injector();
+  var instance = injector.get(Abc);
+  
+  expect(instance, instanceOf(Abc));
+  expect(instance.id, toEqual('abc-id'));
+});
+
+
+it('should resolve basic dependencies', () {
+  var injector = new Injector();
+  var instance = injector.get(Complex);
+
+  expect(instance, instanceOf(Complex));
+  expect(instance.getValue().id, toEqual('abc-id'));
+});
+
+
+it('should allow modules and overriding providers', () {
+  var module = new Module();
+  module.type(Abc, MockAbc);
+  
+  // injector is immutable
+  // you can't load more modules once it's instantiated
+  // (you can create a child injector)
+  var injector = new Injector([module]);
+  var instance = injector.get(Abc);
+  
+  expect(instance.id, toEqual('mock-id'));
+});
+
+
+it('should only create a single instance', () {
+  var injector = new Injector();
+  var first = injector.get(Abc);
+  var second = injector.get(Abc);
+  
+  expect(first, toBe(second));
+});
+
+
+it('should allow providing values', () {
+  var module = new Module();
+  module.value(Abc, 'str value');
+  module.value(Complex, 123);
+
+  var injector = new Injector([module]);
+  var abcInstance = injector.get(Abc);
+  var complexInstance = injector.get(Complex);
+
+  expect(abcInstance, toEqual('str value'));
+  expect(complexInstance, toEqual(123));
+});
+
+
+it('should allow providing factory functions', () {
+  var module = new Module();
+  module.factory(Abc, () {
+    return 'factory-product';
+  });
+
+  var injector = new Injector([module]);
+  var instance = injector.get(Abc);
+
+  expect(instance, toEqual('factory-product'));
+});
+
+
+it('should inject factory function', () {
+  var module = new Module();
+  module.factory(Complex, (Abc abc) {
+    return abc;
+  });
+
+  var injector = new Injector([module]);
+  var instance = injector.get(Complex);
+
+  expect(instance, instanceOf(Abc));
+  expect(instance.id, toEqual('abc-id'));
+});
+
+
+it('should throw an exception when injecting a primitive type', () {
+  var injector = new Injector();
+
+  expect(() {
+    injector.get(NumDependency);
+  }, toThrow(NoProviderException, 'Cannot inject a primitive type of num! ' +
+                                  '(resolving NumDependency -> num)'));
+
+  expect(() {
+    injector.get(BoolDependency);
+  }, toThrow(NoProviderException, 'Cannot inject a primitive type of bool! ' +
+                                  '(resolving BoolDependency -> bool)'));
+
+  expect(() {
+    injector.get(StringDependency);
+  }, toThrow(NoProviderException, 'Cannot inject a primitive type of String! ' +
+                                  '(resolving StringDependency -> String)'));
+});
+
+
+it('should throw an exception when circular dependency', () {
+  var injector = new Injector();
+
+  expect(() {
+    injector.get(CircularA);
+  }, toThrow(CircularDependencyException, 'Cannot resolve a circular dependency! ' +
+                                          '(resolving CircularA -> CircularB -> CircularA)'));
+});
+
+
+// CHILD INJECTORS
+it('should inject from child', () {
+  var module = new Module();
+  module.type(Abc, MockAbc);
+
+  var parent = new Injector();
+  var child = parent.createChild([module]);
+
+  var abcFromParent = parent.get(Abc);
+  var abcFromChild = child.get(Abc);
+
+  expect(abcFromParent.id, toEqual('abc-id'));
+  expect(abcFromChild.id, toEqual('mock-id'));
+});
+
+
+it('should inject instance from parent if not provided in child', () {
+  var module = new Module();
+  module.type(Complex, Complex);
+
+  var parent = new Injector();
+  var child = parent.createChild([module]);
+
+  var complexFromParent = parent.get(Complex);
+  var complexFromChild = child.get(Complex);
+  var abcFromParent = parent.get(Abc);
+  var abcFromChild = child.get(Abc);
+
+  expect(complexFromChild, not(toBe(complexFromParent)));
+  expect(abcFromChild, toBe(abcFromParent));
+});
+
+
+it('should inject instance from parent but never use dependency from child', () {
+  var module = new Module();
+  module.type(Abc, MockAbc);
+
+  var parent = new Injector();
+  var child = parent.createChild([module]);
+
+  var complexFromParent = parent.get(Complex);
+  var complexFromChild = child.get(Complex);
+  var abcFromParent = parent.get(Abc);
+  var abcFromChild = child.get(Abc);
+
+  expect(complexFromChild, toBe(complexFromParent));
+  expect(complexFromChild.getValue(), toBe(abcFromParent));
+  expect(complexFromChild.getValue(), not(toBe(abcFromChild)));
+});
+
+
+it('should force new instance in child even if already instantiated in parent', () {
+  var parent = new Injector();
+  var abcAlreadyInParent = parent.get(Abc);
+
+  var child = parent.createChild([], [Abc]);
+  var abcFromChild = child.get(Abc);
+
+  expect(abcFromChild, not(toBe(abcAlreadyInParent)));
+});
+
+
+it('should force new instance in child using provider from grand parent', () {
+  var module = new Module();
+  module.type(Abc, MockAbc);
+
+  var grandParent = new Injector([module]);
+  var parent = grandParent.createChild([]);
+  var child = parent.createChild([], [Abc]);
+
+  var abcFromGrandParent = grandParent.get(Abc);
+  var abcFromChild = child.get(Abc);
+
+  expect(abcFromChild.id, toEqual(('mock-id')));
+  expect(abcFromChild, not(toBe(abcFromGrandParent)));
+});
+
+}
